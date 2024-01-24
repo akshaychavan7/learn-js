141eea6fd88dd2a5dab5f344a0b6a0c31adc269c
= <=>
2024-01-24 16:18:39 -0500

    arrow functions closure example

--- quiz/scripts/closure.js
+++ quiz/scripts/closure.js
@@ -4,10 +4,10 @@ function setColor(set) {
             let userColor = document.getElementById('color').value;
             document.getElementById('myPara').style.color = userColor;
         }
-        
+    // return setColor;
 }
 
 window.onload = function() {
     let toggle = true;
-    document.getElementById('btn').onclick = setColor(toggle);
+    document.getElementById('btn').onclick = () => setColor(toggle);
 }
