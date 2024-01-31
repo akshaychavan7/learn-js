a0ee0afe663dc06553b2383725c003b4c4c4e4a5
= <=>
2024-01-31 15:34:18 -0500

    string regex excercise

--- quiz/scripts/hideEmail-q.js
+++ quiz/scripts/hideEmail-q.js
@@ -5,7 +5,17 @@ const emails = ['mary@northeastern.edu', 'ari@khoury.northeastern.edu', 'jk_neu.
 window.onload = function hideEmail() {
     const list = document.getElementById('emails')
     list.innerHTML = '';
+    result = []
     for (const email of emails) {
-        // complete the loop
+        if(isValidEmail(email)) {
+            count = email.split('@')[0].length;
+            console.log(count, email)
+            result.push(email.replace(/[a-z|_]+@/, '*'.repeat(count)+'@'))
+        }
     }
+    console.log(result)
+}
+
+function isValidEmail(email) {
+    return /[a-z|_]+@[a-z]+.[a-z]/.test(email)
 }
